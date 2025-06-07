7c4a472e7b1bb397f9a1dd55166f995b6afb357b
Anuroop <anuroopkr13@gmail.com>
2025-06-07 21:15:03 +0530

    event added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,14 @@ import Image from "next/image";
 import Head from "next/head";
 import { motion } from "framer-motion";
 
+const events=[
+  {
+    name:"SSLC/+2 അനുമോദനം",
+    date:"08-06-2025  10:30 AM",
+    location:"Kottoor",
+    image:"/medal.jpg"
+  }
+]
 export default function LandingPage() {
   return (
     <div className="font-sans text-gray-800">
@@ -16,7 +24,7 @@ export default function LandingPage() {
       </Head>
 
       {/* Hero Section */}
-      <section className="relative bg-emerald-800 text-white h-screen flex items-center justify-center text-center overflow-hidden">
+      <section className="relative bg-gray-700 text-white h-screen flex items-center justify-center text-center overflow-hidden">
         <Image
           src="/hero.jpeg"
           alt="Kerala Culture"
@@ -68,8 +76,8 @@ export default function LandingPage() {
       {/* About Us */}
       <section className="py-20 px-6 bg-white text-center">
         <h2 className="text-4xl font-bold mb-6">About Us</h2>
-       <p className="max-w-5xl mx-auto text-gray-700 text-lg mb-6">
-    വിജ്ഞോദയ ആർട്സ് ആൻഡ് സ്പോർട്സ് ക്ലബ്ബ് was founded on <strong>January 1, 2023</strong>, and has quickly grown into one of the most renowned clubs in <strong>Sreekandapuram, Kottoor</strong>. Known for its vibrant energy and active community, the club is dedicated to promoting arts, sports, and cultural unity in the region.
+       <p className="max-w-5xl mx-auto text-gray-900 text-lg mb-6">
+    <strong>വിജ്ഞോദയ ആർട്സ് ആൻഡ് സ്പോർട്സ് ക്ലബ്ബ്</strong>  was founded on <strong>January 1, 2023</strong>, and has quickly grown into one of the most renowned clubs in <strong>Sreekandapuram, Kottoor</strong>. Known for its vibrant energy and active community, the club is dedicated to promoting arts, sports, and cultural unity in the region.
   </p>
   <p className="max-w-5xl mx-auto text-gray-700 text-base">
     Through various programs, events, and tournaments, the club encourages participation from all age groups and continues to be a symbol of creativity, fitness, and togetherness in Kerala.
@@ -93,20 +101,21 @@ export default function LandingPage() {
           Upcoming Events
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {[1, 2, 3].map((event) => (
+          {events.map((event) => (
             <motion.div
-              key={event}
+              key={event.name}
               whileHover={{ scale: 1.03 }}
-              className="p-6 bg-white rounded-xl shadow border"
+              className=" p-10  rounded-xl shadow-xl  bg-cover bg-gray-200"
+              // style={{ backgroundImage: `url('/medal.jpeg')` }}
             >
-              <h3 className="text-2xl font-semibold mb-2">
-                Event Name {event}
+              <h3 className="text-2xl font-semibold mb-2 ">
+                 {event.name}
               </h3>
-              <p className="mb-2">📅 Date & Time</p>
-              <p className="mb-4">📍 Location</p>
-              <button className="px-4 py-2 bg-green-700 text-white rounded hover:bg-green-800 transition">
+              <p className="mb-2">📅 Date & Time:{event.date}</p>
+              <p className="mb-4">📍 Location:{event.location}</p>
+              {/* <button className="px-4 py-2 bg-green-700 text-white rounded hover:bg-green-800 transition">
                 Register
-              </button>
+              </button> */}
             </motion.div>
           ))}
         </div>
